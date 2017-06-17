60ce9e806d2ce4fb306057ae65b5bf13fef50057
ReeceLangerock <ReeceLangerock@gmail.com>
2017-06-17 17:08:15 -0500

    cors

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+
 app.use(function(req, res, next) {
-  if (req.headers["x-forwarded-proto"] === "https") {
-    res.redirect("http://" + req.hostname + req.url);
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    console.log('http://' + req.hostname + req.url);
+    res.redirect('http://' + req.hostname + req.url);
     next();
   } else {
   next();
   }
 });
+
+app.use(express.static("public"));
 
 app.listen(PORT, function() {
   console.log("express server is up on port " + PORT);

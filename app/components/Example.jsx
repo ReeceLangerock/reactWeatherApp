64aac0e2d64d02ea3a7b2236cc24292a633f65fe
ReeceLangerock <ReeceLangerock@gmail.com>
2017-06-17 17:18:13 -0500

    updated example and about page

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,7 +1,12 @@
 var React = require("react");
 
 var Example = props => {
-  return <h3>Exmaple Component</h3>;
+  return (
+    <div>
+      <h3>Example</h3>;
+      <p>Welcome to Example Page</p>
+    </div>
+  );
 };
 
 module.exports = Example;

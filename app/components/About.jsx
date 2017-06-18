51f74fb5172ac0f5af8f4a82ceb329336fb287f5
ReeceLangerock <ReeceLangerock@gmail.com>
2017-06-18 11:02:01 -0500

    add style to weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,32 @@
 var React = require("react");
+var {Link} = require("react-router");
 
 var About = props => {
-  return <div>
+  return (
+    <div>
 
-  <h3>About Component</h3>;
-  <p>Welcome to the About Page</p>
-</div>
+      <div>
+        <h1 className="text-center">About</h1>
+        <p>
+          This is a Weather App built in React following Andrew Mead's Udemy React Course.</p><p> Check out the course
+          {" "}
+          <Link to="https://www.udemy.com/the-complete-react-web-app-developer-course">
+            Here
+          </Link>
+          {" "}
+        </p>
+
+        <p>
+          Check out the GitHub repo
+          {" "}
+          <Link to="https://github.com/ReeceLangerock/reactWeatherApp">
+            Here
+          </Link>
+          {" "}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 module.exports = About;
